bfb95a4cc196728c1cdb1bed2325f943b8f1151b
mswdwk <mswdwk@outlook.com>
2020-11-26 21:41:13 -0500

    fix: set right PREFIX var to solve DEFAULT_CHARSET_HOME error

--- DataBase/MySQL/compiled_and_install/build-libmysqld.sh
+++ DataBase/MySQL/compiled_and_install/build-libmysqld.sh
@@ -1,6 +1,30 @@
 MYSQL_SRC=~/Repos/mysql-server
-#MYSQL_BLD=~/build/build-libmysqld
 MYSQL_ISD=~/MySQL5.7_1
 BOOST_DIR=/usr/local/boost_1_59_0
-#cmake -DCMAKE_INSTALL_PREFIX=$MSQL_ISD  -DDOWNLOAD_BOOST=1 -DWITH_BOOST=$BOOST_DIR $MYSQL_SRC 
-cmake -DCMAKE_INSTALL_PREFIX=$MSQL_ISD  -DWITH_EMBEDDED_SERVER=1 -DWITH_DEBUG=1 -DWITH_BOOST=$BOOST_DIR $MYSQL_SRC 
+BUILD_DIR=~/build-mysql
+
+prepare_make(){
+rm -rf  $BUILD_DIR
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+cmake -DCMAKE_INSTALL_PREFIX=$MYSQL_ISD  -DWITH_EMBEDDED_SERVER=1 -DWITH_DEBUG=1 -DWITH_BOOST=$BOOST_DIR \
+-DINSTALL_LAYOUT=STANDALONE -DDEFAULT_CHARSET=utf8mb4 $MYSQL_SRC 
+}
+
+do_make(){
+	cd $BUILD_DIR
+	make -j16
+}
+
+main(){
+	case $1 in 
+		cmake)
+			prepare_make	
+	;;
+	 	make)
+			do_make	
+	;;
+	esac
+}
+
+main $*
