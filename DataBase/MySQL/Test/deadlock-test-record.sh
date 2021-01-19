8eee09b54859820e9801f868aca58b7b915d4d51
mswdwk <mswdwk@outlook.com>
2021-01-19 07:29:59 -0500

    feat: add mysql-run-test

---
+++ DataBase/MySQL/Test/deadlock-test-record.sh
@@ -0,0 +1,7 @@
+# cd mysql-test
+#./mtr  --extern socket=../bin/mysql1.sock --extern password=Roo123. analyze
+MTR_DIR=~/MySQL5.7_1/mysql-test
+ln -sf `pwd`/t/deadlock.test  $MTR_DIR/t/deadlock.test
+cd $MTR_DIR
+./mtr --record --extern socket=../bin/mysql1.sock --extern password=Roo123.  deadlock
+cd -
