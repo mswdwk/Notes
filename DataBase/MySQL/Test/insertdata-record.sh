0242f58ce13213a70f0f78230897d36614d56783
mswdwk <mswdwk@outlook.com>
2021-02-03 22:23:05 -0500

    test: add insert data test

---
+++ DataBase/MySQL/Test/insertdata-record.sh
@@ -0,0 +1,7 @@
+# cd mysql-test
+#./mtr  --extern socket=../bin/mysql1.sock --extern password=Roo123. analyze
+MTR_DIR=~/MySQL5.7_1/mysql-test
+ln -sf `pwd`/t/insertdata.test  $MTR_DIR/t/insertdata.test
+cd $MTR_DIR
+./mtr --record --extern socket=../bin/mysql1.sock --extern password=Roo123.  insertdata
+cd -
--- DataBase/MySQL/Test/insertdata.sh
+++ DataBase/MySQL/Test/insertdata.sh
@@ -1,7 +1,7 @@
 # cd mysql-test
 #./mtr  --extern socket=../bin/mysql1.sock --extern password=Roo123. analyze
 MTR_DIR=~/MySQL5.7_1/mysql-test
-ln -sf `pwd`/t/deadlock.test  $MTR_DIR/t/deadlock.test
+ln -sf `pwd`/t/insertdata.test  $MTR_DIR/t/insertdata.test
 cd $MTR_DIR
-./mtr --record --extern socket=../bin/mysql1.sock --extern password=Roo123.  deadlock
+./mtr --extern socket=../bin/mysql1.sock --extern password=Roo123.  insertdata
 cd -
