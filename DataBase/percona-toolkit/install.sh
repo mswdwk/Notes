70081fca386aec8116e4594881e0df3a1b0ea33f
mswdwk <mswdwk@outlook.com>
2022-03-12 09:14:43 +0800

    feat: add git modify author method

--- DataBase/percona-toolkit/install.sh
+++ DataBase/percona-toolkit/install.sh
@@ -1,4 +1,6 @@
 
-1) sudo yum install https://repo.percona.com/yum/percona-release-latest.noarch.rpm
+#1)
+sudo yum install https://repo.percona.com/yum/percona-release-latest.noarch.rpm --releasever=7
 
-2)  sudo yum install percona-toolkit --releasever=7
+#2)  
+sudo yum install percona-toolkit --releasever=7
