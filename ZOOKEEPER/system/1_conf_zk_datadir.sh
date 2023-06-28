d4346e489b88ba3eb95b5828d05aab6cfeace8f6
mswdwk <mswdwk@outlook.com>
2023-06-29 05:36:00 +0800

    fix: config zk

--- ZOOKEEPER/system/1_conf_zk_datadir.sh
+++ ZOOKEEPER/system/1_conf_zk_datadir.sh
@@ -1,3 +1,17 @@
-ZK_HOME=$HOME/apache-zookeeper-3.8.1-bin
-sed -i '/dataDir=/d' $ZK_HOME/conf/zoo.cfg 
-sed -i '$a\'"\ndataDir=$ZK_HOME/zook_data" $ZK_HOME/conf/zoo.cfg 
+ZK_DEFAULT_HOME=$HOME/apache-zookeeper-3.8.1-bin
+ZK_NEW_HOME=$HOME/apache-zookeeper-3.8.0-bin
+
+ZK_DEFAULT_DATADIR=$HOME/apache-zookeeper-3.8.1-bin/zook_data
+ZK_NEW_DATADIR=/data1/zookeeper_data
+
+mkdir -p $ZK_NEW_DATADIR
+
+#echo 1 > $ZK_NEW_DATADIR/myid
+
+sed -i '/dataDir=/d' $ZK_NEW_HOME/conf/zoo.cfg 
+sed -i '$a\'"\ndataDir=$ZK_NEW_DATADIR" $ZK_NEW_HOME/conf/zoo.cfg 
+
+sed -i "s?$ZK_DEFAULT_DATADIR?$ZK_NEW_DATADIR?" ./zookeeper.service
+
+sed -i "s?$ZK_DEFAULT_HOME?$ZK_NEW_HOME?" ./zookeeper.service
+
