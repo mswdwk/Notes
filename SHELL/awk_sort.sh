67df89c517707b445b542d097aa7b0f3a52238b6
mswdwk <mswdwk@outlook.com>
2024-03-17 21:51:20 +0800

    feat: awk sort key

--- SHELL/awk_sort.sh
+++ SHELL/awk_sort.sh
@@ -1,2 +1,5 @@
 # 指定数组 降序排序
 awk 'BEGIN{ PROCINFO["sorted_in"]="@val_num_desc";id=0};{arr[id]=$1;id++};END{print "array size=",length(arr);for(i=0;i<length(arr);++i)printf "%s,",arr[i];}'   test_file.txt
+
+# 统计数量排序
+awk 'BEGIN{ PROCINFO["sorted_in"]="@val_num_desc";id=0};{arr[$1]++;id++};END{print "array size=",length(arr);for(k in arr)printf "%s count %s,",k,arr[k];}'   test_file.txt
